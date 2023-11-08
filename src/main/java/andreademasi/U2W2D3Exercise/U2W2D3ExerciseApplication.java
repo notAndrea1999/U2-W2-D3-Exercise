7ef09d388e776d04b2c89fed9298665b0f965dc8
Andrea De Masi <andreademasi1999@gmail.com>
2023-11-08 14:13:51 +0100

    Initial commit

---
+++ src/main/java/andreademasi/U2W2D3Exercise/U2W2D3ExerciseApplication.java
@@ -0,0 +1,13 @@
+package andreademasi.U2W2D3Exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W2D3ExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W2D3ExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andreademasi/U2W2D3Exercise/U2W2D3ExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package andreademasi.U2W2D3Exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W2D3ExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
